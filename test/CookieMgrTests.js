184e00e9bb36a2c306e3faf6d179cd6e3a848238
Christian Dalager <christian@dalager.com>
2013-09-24 15:46:15 +0200

    removed deactivated cookiemgr test

--- test/CookieMgrTests.js
+++ test/CookieMgrTests.js
@@ -16,20 +16,13 @@ test('readCookie will return null on empty string', function () {
 test('readCookie will return null on null', function () {
     var val = CookieMgr.readCookie();
     equal(val, null);
-});;
-
+});
 
 test('createCookie will create cookie', function () {
     CookieMgr.createCookie('asdf', 'testval', 20);
     var saved = CookieMgr.readCookie('asdf');
     equal(saved, 'testval');
 });
-
-//test('document cookie works',function() {
-//    equal(document.cookie, '','There should not be any cookies.');
-//    document.cookie='test=123';
-//    equal(document.cookie, 'test=123','Expected cookie');
-//});
 
 test('eraseCookie will survive non-existing cookie', function () {
     CookieMgr.eraseCookie('test');
