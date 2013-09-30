5c8418a49a14177642465e6bbee4dcbd4b2b55d4
Christian Dalager <christian@dalager.com>
2013-09-30 09:33:32 +0200

    cleanup

--- test/CookiePrompterTests.js
+++ test/CookiePrompterTests.js
@@ -8,66 +8,69 @@ test('Init can run', function() {
     notEqual(null, Document);
 });
 
-
-test('TestTracker is initialized', function() {
+test('TestTracker is initialized', function () {
     expect(1);
     CookiePrompter.init({
         trackers: [{
             name: TestTracker,
-            config: { ready:function() {
-                ok(true, 'tracker inialized');
-            }}
+            config: {
+                ready: function () {
+                    ok(true, 'tracker inialized');
+                }
+            }
         }]
     });
 });
 
-
-
-test('TestTracker will not inject if first visit',function(){
+test('TestTracker will not inject if first visit', function() {
     CookiePrompter.removeCookies();
     CookieMgr.eraseCookie('cookieOptOut');
     CookiePrompter.init({
-        cameFromSameDomain: function(doc){return false;},
+        cameFromSameDomain: function(doc) { return false; },
         trackers: [{
             name: TestTracker,
-            config: { ready:function() {}}
+            config: {
+                ready: function() {
+                }
+            }
         }]
-    }); 
+    });
     var el = document.getElementById('h1header');
-    ok(el===null, 'Code injected, but should not have been');
-})
+    ok(el === null, 'Code injected, but should not have been');
+});
 
-test('TestTracker will inject code if referrer from same domain',function(){
+test('TestTracker will inject code if referrer from same domain', function() {
     CookiePrompter.removeCookies();
     CookieMgr.eraseCookie('cookieOptOut');
     CookiePrompter.init({
-        cameFromSameDomain: function(doc){return true;},
+        cameFromSameDomain: function(doc) { return true; },
         trackers: [{
             name: TestTracker,
-            config: { ready:function() {}}
+            config: {
+                ready: function() {
+                }
+            }
         }]
-    }); 
+    });
     var el = document.getElementById('h1header');
     ok(el, 'Code not injected, but should not have been');
-})
+});
 
 
-test('Explicit consent will be respected even if from same domain',function(){
+test('Explicit consent will be respected even if from same domain', function() {
     CookiePrompter.removeCookies();
     CookieMgr.eraseCookie('cookieOptOut');
     CookiePrompter.init({
-        cameFromSameDomain: function(doc){return true;},
+        cameFromSameDomain: function(doc) { return true; },
         explicitAccept: true,
         trackers: [{
             name: TestTracker,
-            config: { ready:function() {}}
+            config: {
+                ready: function() {
+                }
+            }
         }]
-    }); 
+    });
     var el = document.getElementById('h1header');
-    ok(el===null, 'Code injected, but should not have been');
-})
-
-
-
-
-
+    ok(el === null, 'Code injected, but should not have been');
+});
