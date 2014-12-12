eb53cc653116c642412eed69eea5643268920c21
Christian Dalager <christian@dalager.com>
2014-12-12 15:33:52 +0100

    beautify

--- test/CookiePrompterTests.js
+++ test/CookiePrompterTests.js
@@ -2,28 +2,31 @@
 /// <reference path="resources/qunit.js" />
 
 
-function cleanup(){
+function cleanup() {
     var existingPrompts = document.getElementsByClassName('eksCookieContainer');
-    if(existingPrompts.length>0){
-        for(var i=0;i<existingPrompts.length;i++)
-            {
-                if(existingPrompts[i])
-                    existingPrompts[i].parentNode.removeChild(existingPrompts[i]);
+    if (existingPrompts.length > 0) {
+        for (var i = 0; i < existingPrompts.length; i++) {
+            if (existingPrompts[i])
+                existingPrompts[i].parentNode.removeChild(existingPrompts[i]);
 
-            };
+        };
     }
 }
 
 
 
-module('CookiePrompter tests',{
-    setup:function(){
-        CookiePrompter.init({enableLog:false}); // reset defaults
+module('CookiePrompter tests', {
+    setup: function() {
+        CookiePrompter.init({
+            enableLog: false
+        }); // reset defaults
         cleanup();
     },
-    teardown: function(){
+    teardown: function() {
         CookieMgr.eraseCookie('cookieOptOut');
-        CookiePrompter.init({enableLog:false}); // reset defaults
+        CookiePrompter.init({
+            enableLog: false
+        }); // reset defaults
         cleanup();
     }
 });
@@ -34,13 +37,13 @@ test('Init can run', function() {
 });
 
 
-test('OnOptOut callback can be null',function(){
+test('OnOptOut callback can be null', function() {
     expect(2);
     CookiePrompter.init({
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function () {
+                ready: function() {
                     ok(true, 'tracker inialized');
                     CookiePrompter.eraseCookiesAndRemovePrompt();
                     ok(true);
@@ -51,17 +54,17 @@ test('OnOptOut callback can be null',function(){
 });
 
 
-test('config keys will be set individually for each init',function(){
+test('config keys will be set individually for each init', function() {
     expect(4);
     CookiePrompter.init({
         trackLandingPage: true,
-        onReady: function(cfg){
-            ok(cfg.trackLandingPage===true);
+        onReady: function(cfg) {
+            ok(cfg.trackLandingPage === true);
         },
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function () {
+                ready: function() {
                     ok(true, 'tracker inialized');
                 }
             }
@@ -69,13 +72,13 @@ test('config keys will be set individually for each init',function(){
     });
 
     CookiePrompter.init({
-        onReady: function(cfg){
-            ok(cfg.trackLandingPage===false);
+        onReady: function(cfg) {
+            ok(cfg.trackLandingPage === false);
         },
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function () {
+                ready: function() {
                     ok(true, 'tracker inialized');
                 }
             }
@@ -83,34 +86,34 @@ test('config keys will be set individually for each init',function(){
     });
 });
 
-test('OnOptOut callback will be called once when erasing cookies',function(){
+test('OnOptOut callback will be called once when erasing cookies', function() {
     expect(2);
     var callCount = 0;
     CookiePrompter.init({
         enableLog: false,
-        onOptOut: function(href){
-            callCount = callCount+1;
+        onOptOut: function(href) {
+            callCount = callCount + 1;
         },
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function () {
+                ready: function() {
                     ok(true, 'tracker inialized');
                 }
             }
         }]
     });
     CookiePrompter.eraseCookiesAndRemovePrompt();
-    ok(callCount===1);
+    ok(callCount === 1);
 });
 
-test('UnitTestTracker is initialized', function () {
+test('UnitTestTracker is initialized', function() {
     expect(1);
     CookiePrompter.init({
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function () {
+                ready: function() {
                     ok(true, 'tracker inialized');
                 }
             }
@@ -124,8 +127,7 @@ test('UnitTestTracker will not inject if first visit', function() {
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function() {
-                }
+                ready: function() {}
             }
         }]
     });
@@ -134,45 +136,44 @@ test('UnitTestTracker will not inject if first visit', function() {
 });
 
 
-test("Implicit accept, no cookies and referer from same domain will set cookie",function(){
+test("Implicit accept, no cookies and referer from same domain will set cookie", function() {
     expect(1);
 
-    var fakeCookieMgr = (function(){
-        return{
-            init:function(){},
-            createCookie:function(name,value,days){
-                ok(true,"Expected cookie to be created");
+    var fakeCookieMgr = (function() {
+        return {
+            init: function() {},
+            createCookie: function(name, value, days) {
+                ok(true, "Expected cookie to be created");
             },
-            readCookie:function(){
-            }
-    };
+            readCookie: function() {}
+        };
     })();
 
     CookiePrompter.init({
-        cookieMgr:fakeCookieMgr,
+        cookieMgr: fakeCookieMgr,
         referrerHandler: SameDomainReferrerHandler,
     });
 });
 
-test("Cookie will use provided expirydays when overridden",function(){
+test("Cookie will use provided expirydays when overridden", function() {
     expect(1);
 
-    var fakeCookieMgr = (function(){
-        return{
-            init:function(){},
+    var fakeCookieMgr = (function() {
+        return {
+            init: function() {},
 
-            createCookie:function(name,value,days){
-                ok(days===45,"ExpiryDays not correctly provided");
+            createCookie: function(name, value, days) {
+                ok(days === 45, "ExpiryDays not correctly provided");
             },
-            readCookie:function(){
+            readCookie: function() {
                 return null;
             }
         };
     })();
 
     CookiePrompter.init({
-        expiryDays:45,
-        cookieMgr:fakeCookieMgr,
+        expiryDays: 45,
+        cookieMgr: fakeCookieMgr,
         referrerHandler: SameDomainReferrerHandler,
     });
 });
@@ -181,12 +182,11 @@ test("Cookie will use provided expirydays when overridden",function(){
 test('UnitTestTracker will inject code if referrer from same domain', function() {
     CookiePrompter.init({
         referrerHandler: SameDomainReferrerHandler,
-        enableLog:false,
+        enableLog: false,
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function() {
-                }
+                ready: function() {}
             }
         }]
     });
@@ -195,14 +195,14 @@ test('UnitTestTracker will inject code if referrer from same domain', function()
 });
 
 test('Finding OK cookie will result in code injection', function() {
-    var fakeCookieMgr = (function(){
-        return{
-                        init:function(){},
+    var fakeCookieMgr = (function() {
+        return {
+            init: function() {},
 
-            createCookie:function(name,value,days){
+            createCookie: function(name, value, days) {
                 throw "Should not create cookie, when it exists";
             },
-            readCookie:function(){
+            readCookie: function() {
                 return "y";
             }
         };
@@ -210,12 +210,11 @@ test('Finding OK cookie will result in code injection', function() {
     CookiePrompter.init({
         referrerHandler: SameDomainReferrerHandler,
         cookieMgr: fakeCookieMgr,
-        enableLog:false,
+        enableLog: false,
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function() {
-                }
+                ready: function() {}
             }
         }]
     });
@@ -224,14 +223,14 @@ test('Finding OK cookie will result in code injection', function() {
 });
 
 test('Finding OK cookie will result in code injection', function() {
-    var fakeCookieMgr = (function(){
-        return{
-                        init:function(){},
+    var fakeCookieMgr = (function() {
+        return {
+            init: function() {},
 
-            createCookie:function(name,value,days){
+            createCookie: function(name, value, days) {
                 throw "Should not create cookie, when it exists";
             },
-            readCookie:function(){
+            readCookie: function() {
                 return "n";
             }
         };
@@ -239,17 +238,16 @@ test('Finding OK cookie will result in code injection', function() {
     CookiePrompter.init({
         referrerHandler: SameDomainReferrerHandler,
         cookieMgr: fakeCookieMgr,
-        enableLog:false,
+        enableLog: false,
         trackers: [{
             name: UnitTestTracker,
             config: {
-                ready: function() {
-                }
+                ready: function() {}
             }
         }]
     });
     var el = document.getElementById('h1header');
-    ok(el===null, 'NO cookie set, but tracker injected code anyway');
+    ok(el === null, 'NO cookie set, but tracker injected code anyway');
 });
 
 test('Explicit consent will be respected even if from same domain', function() {
@@ -261,7 +259,7 @@ test('Explicit consent will be respected even if from same domain', function() {
             config: {
                 ready: function() {
 
-                }   
+                }
             }
         }]
     });
@@ -269,54 +267,65 @@ test('Explicit consent will be respected even if from same domain', function() {
     ok(el === null, 'Code injected, but should not have been');
 });
 
-test('will show cookieprompt if no cookies present',function(){
+test('will show cookieprompt if no cookies present', function() {
     CookiePrompter.init({});
     var cookiePrompt = document.getElementById('eksCookiePrompt');
-    ok(cookiePrompt,'CookiePrompt was not rendered');    
+    ok(cookiePrompt, 'CookiePrompt was not rendered');
 });
 
 
-test('Will show OK button if given in config',function(){
-    CookiePrompter.init({showOKbutton: true});
+test('Will show OK button if given in config', function() {
+    CookiePrompter.init({
+        showOKbutton: true
+    });
     var okbtns = document.getElementsByClassName('cpAcceptBtn');
-    ok(okbtns.length===1,'No ok button was rendered');
+    ok(okbtns.length === 1, 'No ok button was rendered');
 });
 
-test('Will not show OK button when told not to',function(){
-    CookiePrompter.init({showOKbutton: false});
+test('Will not show OK button when told not to', function() {
+    CookiePrompter.init({
+        showOKbutton: false
+    });
     var okbtns = document.getElementsByClassName('cpAcceptBtn');
-    ok(okbtns.length===0,'Ok button was rendered, shouldnt be');
+    ok(okbtns.length === 0, 'Ok button was rendered, shouldnt be');
 });
 
-test('Will not show OK button by default',function(){
+test('Will not show OK button by default', function() {
     CookiePrompter.init({});
     var okbtns = document.getElementsByClassName('cpAcceptBtn');
-    ok(okbtns.length===0,'Ok button was rendered, shouldnt be');
+    ok(okbtns.length === 0, 'Ok button was rendered, shouldnt be');
 });
 
 
-test('Default text on OK btn will be OK',function(){
-    CookiePrompter.init({showOKbutton:true});
+test('Default text on OK btn will be OK', function() {
+    CookiePrompter.init({
+        showOKbutton: true
+    });
     var btn = document.getElementsByClassName('cpAcceptBtn')[0];
-    ok(btn.innerText==='OK','Text on OK button not OK. Literally.');
+    ok(btn.innerText === 'OK', 'Text on OK button not OK. Literally.');
 });
 
-test('Supplied text for OK btn will be used',function(){
-    CookiePrompter.init({showOKbutton:true,textOKbutton:'yeah!'});
+test('Supplied text for OK btn will be used', function() {
+    CookiePrompter.init({
+        showOKbutton: true,
+        textOKbutton: 'yeah!'
+    });
     var btn = document.getElementsByClassName('cpAcceptBtn')[0];
-    ok(btn.innerText==='yeah!','Text on OK button was not taken from config, was "'+btn.innerText+'"');
+    ok(btn.innerText === 'yeah!', 'Text on OK button was not taken from config, was "' + btn.innerText + '"');
 });
 
-test('Will not show explicitAccept buttons when not activated',function(){
+test('Will not show explicitAccept buttons when not activated', function() {
     CookiePrompter.init({});
     var okbtns = document.getElementsByClassName('cpAcceptBtn');
-    var nobtns= document.getElementsByClassName('cpDontAcceptBtn');
-    ok(okbtns.length===0 && nobtns.length===0,'Explicit accept buttons were rendered, shouldnt be');
+    var nobtns = document.getElementsByClassName('cpDontAcceptBtn');
+    ok(okbtns.length === 0 && nobtns.length === 0, 'Explicit accept buttons were rendered, shouldnt be');
 });
 
-test('Will show explicitAccept buttons when explicitAccept is set to true',function(){
-    CookiePrompter.init({explicitAccept:true});
+test('Will show explicitAccept buttons when explicitAccept is set to true', function() {
+    CookiePrompter.init({
+        explicitAccept: true
+    });
     var okbtns = document.getElementsByClassName('cpAcceptBtn');
-    var nobtns= document.getElementsByClassName('cpDontAcceptBtn');
-    ok(okbtns.length===1 && nobtns.length===1,'Explicit accept buttons were not rendered');
+    var nobtns = document.getElementsByClassName('cpDontAcceptBtn');
+    ok(okbtns.length === 1 && nobtns.length === 1, 'Explicit accept buttons were not rendered');
 });
