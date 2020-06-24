b95a8d561ad68607962261c6aaaf43b2f8746e7e
Mads Mørch Nielsen <mads.moerch@eksponent.com>
2020-06-24 11:38:02 +0200

    Move server.js to samples/sampleserver.js
    Fix static path for the sample server

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.use(express.static(__dirname));
-
-app.listen(port, () => console.log("Running on http://localhost:3000/samples"));
-
-
